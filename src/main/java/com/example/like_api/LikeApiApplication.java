099488358966e00a9395666346a29f46aff3f607
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-03-27 14:11:31 +0300

    Initial commit

---
+++ src/main/java/com/example/like_api/LikeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.like_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/like_api/LikeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.like_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
